eded3d2b88c24059e82a0a350b4285cf1540bd63
Rehan <rehanpathan8012@gmail.com>
2024-04-14 20:14:56 +0530

    added backend

---
+++ public/signup.js
@@ -0,0 +1,57 @@
+const signUpForm = document.getElementsByClassName("signup-form")[0];
+// const loginForm = document.getElementsByClassName("login-form")[0];
+
+signUpForm.addEventListener("submit", async function (event) {
+  event.preventDefault();
+
+  const formData = {
+    username: this.elements.username.value,
+    email: this.elements.email.value,
+    password: this.elements.password.value,
+  };
+
+  try {
+    const response = await fetch("http://127.0.0.1:5000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+    // Display the message in an alert
+    alert(data.message);
+    if (data.message === "Signup successful!") location.href = "login.html";
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// loginForm.addEventListener("submit", async function (event) {
+//   event.preventDefault();
+
+//   const formData = {
+//     username: this.elements.username.value,
+//     password: this.elements.password.value,
+//   };
+//   console.log(formData);
+
+//   try {
+//     const response = await fetch("http://127.0.0.1:5000/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+
+//     const data = await response.json();
+//     // Display the message in an alert
+//     console.log(data);
+//     alert(data.message);
+//     if (data.message === "Login successful!") location.href = "index.html";
+//   } catch (err) {
+//     console.log(err);
+//   }
+// });
